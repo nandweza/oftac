efa0d32cc4906a928e148b2930e277b990ecc2a9
nandweza <allannandweza@gmail.com>
2023-08-25 21:54:42 +0300

    updated navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,11 @@
 import './navbar.css';
 import Logo from '../../assets/oftac.png';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand px-5" href="/">
+            <Link to='/' className="navbar-brand px-5">
                 <img 
                     src={Logo}
                     alt='oftac logo'
@@ -12,33 +13,33 @@ const Navbar = () => {
                     height='90'
                     className='logo'
                 />
-            </a>
+            </Link>
             <button className="navbar-toggler mx-5" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav ml-auto justify-content-end px-5">
                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Home</a>
+                        <Link to='/' className="nav-link">Home</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/about">About</a>
+                        <Link to='/about' className="nav-link">About</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/projects">Projects</a>
+                        <Link to='/projects' className="nav-link">Projects</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/blog">News</a>
+                        <Link to='/blog' className="nav-link">News</Link>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="/contact">Contact</a>
+                        <Link to='/contact' className="nav-link">Contact</Link>
                     </li>
                     <li>
-                        <a className="nav-link" href="/#">
+                        <Link to='#' className="nav-link">
                             <button className='donate-btn'>
                                 Donate
                             </button>
-                        </a>
+                        </Link>
                     </li>
                 </ul>
             </div>
